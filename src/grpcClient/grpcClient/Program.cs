e63043d32ff586f27cf7b79ac62aa6f413284d81
AhmetVDemir <mumaru171@gmail.com>
2023-06-14 14:40:32 +0300

    grpc base generated

---
+++ src/grpcClient/grpcClient/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+using Grpc.Net.Client;
+using grpcServer;
+
+var channel = GrpcChannel.ForAddress("http://localhost:5278/");
+var greetClient = new Greeter.GreeterClient(channel);
+
+HelloReply hello =  greetClient.SayHello(new HelloRequest { Name = "Selam Ahmet" });
+
+Console.WriteLine(hello.Message);
